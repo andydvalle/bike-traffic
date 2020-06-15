68ffda8259c27b681f6353e90fbe1a8414589758
github username <github email address>
2020-06-15 10:48:41 -0700

    adds basic message feature

--- bike-traffic-api/app/controllers/messages_controller.rb
+++ bike-traffic-api/app/controllers/messages_controller.rb
@@ -1,2 +1,9 @@
 class MessagesController < ApplicationController
+    # Adds index to render json localhost:3000/messages
+    def index
+        messages = Message.all
+        render json: messages.to_json(:include => {
+            :bike_trail => {:only => [:name, :distance, :counter_location]}
+        }, :except => [:created_at, :updated_at])
+    end
 end
--- bike-traffic-api/db/seeds.rb
+++ bike-traffic-api/db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# (ANDY) created trails and messages for test of message section
+trail1 = BikeTrail.create(name: "trail 1", distance: 1 ,counter_location: "counter_location 1")
+trail2 = BikeTrail.create(name: "trail 2", distance: 2 ,counter_location: "counter_location 2")
+trail3 = BikeTrail.create(name: "trail 3", distance: 3 ,counter_location: "counter_location 3")
+
+message1 = Message.create(user_name: "user1", content: "content1", bike_trail: trail1)
+message2 = Message.create(user_name: "user2", content: "content2", bike_trail: trail2)
+message3 = Message.create(user_name: "user3", content: "content3", bike_trail: trail3)
